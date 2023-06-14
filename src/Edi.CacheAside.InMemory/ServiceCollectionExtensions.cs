595305efe0b8ebb76cefeca6ec5f7847be5864f3
Edi Wang <Edi.Wang@outlook.com>
2023-06-14 10:58:06 +0800

    add code files

---
+++ src/Edi.CacheAside.InMemory/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Edi.CacheAside.InMemory;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInMemoryCacheAside(this IServiceCollection services)
+    {
+        services.AddMemoryCache();
+        services.AddSingleton<ICacheAside, MemoryCacheAside>();
+        return services;
+    }
+}
